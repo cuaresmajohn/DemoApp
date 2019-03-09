b4a3cc29a153f0b218e1483d5d4a82219d79aa74
cuaresmajohn <cuaresmajohn@gmail.com>
2019-03-09 22:57:11 +0800

    Initial Change

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { from } from 'rxjs';
 
 @NgModule({
    declarations: [
@@ -10,7 +11,8 @@ import { ValueComponent } from './value/value.component';
       ValueComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
